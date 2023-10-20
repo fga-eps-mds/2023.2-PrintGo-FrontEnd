73ac023ff3d0dd532203e224862f0a59b91196d4
julisous <julia.farias2000@hotmail.com>
2023-10-20 11:49:25 -0300

    :bulb:(#149) Configurando pastas iniciais

---
+++ src/assets/theme.md
@@ -0,0 +1,7 @@
+Para tamanho de div : vw e vh 
+Para tamanho de fonte: px
+Para tamanho de tela: %
+Fonte: Roboto para texto
+       Jost para título
+
+       
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../components/Navbar"
+import home from "../style/pages/home.css"
+
+
+export default function Home(){
+
+return(
+   
+    <div className="container">
+         
+    </div>
+    
+
+
+);
+
+
+
+
+}
