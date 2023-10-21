18e61c4fae6f4234e089a80d0e5345cf068e8d66
ThornColli <eduardodemelocolli@gmail.com>
2023-10-21 01:48:32 -0300

    :bulb: (#149) Criando barra de navegação

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,37 @@
-import navbar from "../style/components/navbar.css"
+import "../style/components/navbar.css"
+import React from "react";
+import logo from "../assets/logo 3.svg";
+
+export default function Navbar(){
+
+return(
+   
+    <div className="fundo">
+        <div className="container">
+            <img src={logo}></img>
+            <div className="Botoes"> 
+                <button>
+                    Home
+                </button>
+                <button>
+                    Quem somos
+                </button>
+                <button>
+                    Contato
+                </button>
+            </div>
+            
+            <button className="Login">
+                Login
+            </button>
+        </div>
+    </div>
+    
+
+
+);
+
+
+
+
+}
