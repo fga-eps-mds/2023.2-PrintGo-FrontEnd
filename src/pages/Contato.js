1a717bdbbd9aa451aded9e9b3a456498aed39431
Yuri Alves <yurivalerio12@gmail.com>
2023-10-21 16:12:48 -0300

    :bulb:(#154): criação pagina de contato
    Co-authored-by:  Brenno-Silva01 <Brenno877@gmail.com>

---
+++ src/pages/Contato.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "../style/pages/contato.css"
+import Imagem_Homen from "../assets/imagem-homen-ruivo.svg";
+import Input from "../components/Input"
+
+
+export default function Contato(){
+
+return(
+   
+    <div className="Main-page">
+        <div className="Contato-principal">
+            <h1>Contato</h1>
+            <div className="Box-contato">
+                <p>Contato</p>
+            </div>
+            <Input label="Nome:" placeholder="Digite seu nome" />
+            <button className="Enviar-button">Enviar</button>
+        </div>
+        <div className="Imagem">
+            <img src={Imagem_Homen} alt="Imagem de um homem ruivo"/>
+        </div> 
+    </div>
+    
+);
+
+}
