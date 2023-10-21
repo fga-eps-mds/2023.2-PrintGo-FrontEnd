a9aa63f8837d397c7b9744fadae1312222a9a80f
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2023-10-21 14:08:30 -0300

    :bulb:(#150): melhoria do layout das paginas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import '../style/pages/login.css'; 
-import pessoas from '../assets/pessoas.png';
+import pessoas from '../assets/pessoas.svg';
 
 function Login() {
   return (
-    <div className="login-container">
-      <img src={pessoas} alt="Pessoas" className="pessoas" />
-      <div className="login-box">
-        <div className="login-box-content">
-          <h2>Entrar</h2>
-          <form>
-            <div className="form-group">
-              <label htmlFor="username">e-mail</label>
-              <input type="text" id="username" name="username" />
+    <body>
+      <div className="container">
+        <img src={pessoas} alt="Pessoas" className="pessoas" />
+        <div>
+          <div className="ellipse"/>
+          <div className="login-box">
+            <div className="login-box-content">
+              <h2>Entrar</h2>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="username">e-mail</label>
+                  <input type="text" id="username" name="username" placeholder="email@email.com" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">senha</label>
+                  <input type="password" id="password" name="password" placeholder="************" />
+                </div>
+                <div className="buttons">
+                  <div className="button-container">
+                    <button type="submit">LOGIN</button>
+                  </div>
+                  <div className="button-container">
+                    <button type="button">Recuperar senha</button>
+                  </div>
+                </div>
+              </form>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">senha</label>
-              <input type="password" id="password" name="password" />
-            </div>
-            <button type="submit">LOGIN</button>
-            <button type="button">Recuperar senha</button>
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </body>
   );
 }
 
