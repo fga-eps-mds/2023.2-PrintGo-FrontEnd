9f7226e9ec0bf73e9b45229625af023db4a18b3c
leticiatmartins <leticiats02@gmail.com>
2023-10-20 22:04:26 -0300

    💡(#150): implementacao pagina de login
    Co-authored-by: Guilherme Fernandes <guilhermedfs11@gmail.com>

---
+++ src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/pages/login.css'; 
+import pessoas from '../assets/pessoas.png';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-box-content">
+          <h2>Entrar</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
