0e3f488c3cc93d1d0bc7099f692e461deac223d0
Natanael Fernandes <filhonatanael01@gmail.com>
2023-10-21 22:48:54 -0300

    :pencil: (#151): add forms structure
    Co-authored-by: DaviMatheus <matdavimat@gmail.com>

---
+++ src/components/SignupForm.js
@@ -0,0 +1,39 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export default function SignupForm(){
+    const {
+        register,
+        handleSubmit,
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+    return(
+        <div>      
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                <input {...register("nome")} placeholder="Nome" />
+                </div>
+                <div>
+                <input {...register("email")} placeholder="Email" />
+                </div>
+                <div><input {...register("emailCofirmar")} placeholder="Confirmar Email" /></div>
+                <div><input {...register("senha")} placeholder="Senha" type="password"/></div>
+                <div><input {...register("senhaConfirmar")} placeholder="Confirmar Senha" type="password"/></div>
+                <div><input {...register("documento")} placeholder="CPF ou CNPF" /></div>
+                <div><input {...register("password")} placeholder="Senha" /></div>
+                <div><input {...register("lotacao_id")} placeholder="Lotacao" /></div>
+                <div><input {...register("cargos")} placeholder="Cargo" /></div>
+                <div>
+                    <button type="button">CANCELAR</button>
+                    <button type="submit">REGISTAR</button>
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ src/pages/CreateUser.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SignupForm from "../components/SignupForm";
+
+
+export default function CreateUserPage(){
+
+return(
+   
+    <div className="container">
+         <SignupForm/>
+    </div>
+    
+
+
+);
+
+
+
+
+}
