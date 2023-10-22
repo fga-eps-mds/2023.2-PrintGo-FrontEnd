10431b4355340c8a65a95c99c5baec6001d041a9
vitordiniz25 <vitordiniz25@gmail.com>
2023-10-22 11:30:21 -0300

    :bulb:(#153): Arrumando lint da pag home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,67 +7,38 @@ import elipse from '../assets/home_elipse.svg';
 
 
 export default function Home(){
-
-return(
-
-<div className="homepage">
-    
-    <div className="nav-bar"></div>
-
-    <div className="content">
-
-        <div className="left-content">
-
-            <div className="aligned-content">
-                <div className="page-text">
-                    <b className="printgo">PrintGo</b>
-            
-                    <div className="home-text">
-                        Contando impressões desde 2023
+    return(
+            <div className="homepage">
+                <div className="nav-bar"></div>
+                <div className="content">
+                    <div className="left-content">
+                        <div className="aligned-content">
+                            <div className="page-text">
+                                <b className="printgo">PrintGo</b>
+                                <div className="home-text">
+                                    Contando impressões desde 2023
+                                </div>
+                            </div>         
+                            <div className="containter-button">
+                                <div className="saiba-mais">
+                                    <Button
+                                        textColor="#036"
+                                        bgColor="white"
+                                        borderColor="#036"
+                                    >
+                                        Saiba mais
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="elipse">
+                            <img alt ="" src={elipse}></img>
+                        </div>
+                    </div>
+                    <div className="right-content">
+                        <img className="home-image" alt="" src={home_image} />
                     </div>
                 </div>
-            
-                <div className="containter-button">
-                    <div className="saiba-mais">
-                        <Button
-                            textColor="#036"
-                            bgColor="white"
-                            borderColor="#036"
-
-                        >
-                            Saiba mais
-                        </Button>
-                    
-                    </div>
-
-                </div>
-                
-
             </div>
-            
-            <div className="elipse">
-                <img alt ="" src={elipse}></img>
-            </div>
-
-            
-        </div>
-
-        <div className="right-content">
-            <img className="home-image" alt="" src={home_image} />
-        </div>
-
-    </div>
-
-    
-    
-</div>
-
-    
-
-
-);
-
-
-
-
+        );
 }
