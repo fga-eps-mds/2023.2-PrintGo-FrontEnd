7c6f2c611960302860a61c23ec8263948ecccf5a
Júlia Farias Sousa <julia@MacBook-Pro-de-Julia.local>
2023-10-22 15:22:48 -0300

    :bug: Corrigindo navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter, Routes , Route} from "react-router-dom"
+import NavbarSimple from "./components/NavbarSimple"
 import Navbar from "./components/Navbar"
 
 function App(){
@@ -7,6 +8,7 @@ function App(){
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/NavbarSimple" element={<NavbarSimple/>}/>
       <Route path="/Navbar" element={<Navbar/>}/>
     </Routes>
     </BrowserRouter>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import "../style/components/navbar.css"
 import React from "react";
 import logo from "../assets/logo 3.svg";
+import chevron from "../assets/chevron-down.svg";
+import email from "../assets/email.svg"
+import sino from "../assets/sino.svg"
+import user from "../assets/user.svg"
+
 
 export default function Navbar(){
 
@@ -19,11 +24,30 @@ return(
                 <button>
                     Contato
                 </button>
+                <button>
+                    Cadastros
+                </button>
+                <button>
+                    Contagens
+                </button>
             </div>
             
-            <button className="Login">
-                Login
+            <div className="Icons">
+               <button>
+            <img src={sino}></img>
             </button>
+            <button>
+            <img src={email}></img>
+            </button> 
+            </div>
+
+            <div className="User">
+            <button>
+            <img src={user}></img>
+            <p>User</p>
+            <img src={chevron}></img> 
+            </button>
+            </div>
         </div>
     </div>
     
