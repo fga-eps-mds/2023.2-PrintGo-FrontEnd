e238d4663a47ceadd349f47b16102a61fb1666e5
Yuri Alves <yurivalerio12@gmail.com>
2023-10-22 20:22:56 -0300

    :bulb:(#154): funcao de enviar email online

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -2,18 +2,36 @@ import React, { useState } from 'react';
 import "../style/pages/contato.css";
 import Imagem_Homen from "../assets/imagem-homen-ruivo.svg";
 import Input from "../components/Input";
+import emailjs from "@emailjs/browser";
 
 export default function Contato() {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [assunto, setAssunto] = useState('');
 
-  const handleEnviar = () => {
+  function handleEnviar(e){
     // Aqui você pode lidar com os dados, como validação e envio para um servidor
     console.log("Nome:", nome);
     console.log("E-mail:", email);
     console.log("Assunto:", assunto);
-  };
+
+    const templateParams = {
+        from_name: nome,
+        email: email,
+        message: assunto
+    }
+
+    emailjs.send("service_amwcg9n","template_5l6g71s",templateParams,"az5Vq1c-iMacr4p-z")
+    .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setAssunto('');
+        setEmail('');
+        setNome('');
+    }, (err) => {
+        console.log('FAILED...', err);
+
+    })
+  }
 
   return (
     <div className="Main-page">
