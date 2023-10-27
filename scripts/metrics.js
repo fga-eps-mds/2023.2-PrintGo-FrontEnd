0572db5ddea2062e71cfa1ac71e139d7003f673f
DaviMatheus <matdavimat@gmail.com>
2023-10-26 22:30:00 -0300

    :bug: finxing_sonar
    Co-authored-by: fernandes-natanael <filhonatanael01@gmail.com>

--- scripts/metrics.js
+++ scripts/metrics.js
@@ -1,28 +1,26 @@
 const REPO = '2023.2-PrintGo-FrontEnd'
-const SONAR_OWNER = 'fga-eps-mds-1'
 const OWNER = 'fga-eps-mds'
 const SONAR_ID = 'fga-eps-mds_2023.2-PrintGo-FrontEnd'
-
 const METRIC_LIST = [
-  'files',
-  'functions',
-  'complexity',
-  'comment_lines_density',
-  'duplicated_lines_density',
-  'coverage',
-  'ncloc',
-  'tests',
-  'test_errors',
-  'test_failures',
-  'test_execution_time',
-  'security_rating'
-]
-const SONAR_URL = `https://sonarcloud.io/api/measures/component_tree?component=${SONAR_ID}&metricKeys=${METRIC_LIST.join(
-  ','
-)}&ps=500`
+
+    'files',
+    'functions',
+    'complexity',
+    'comment_lines_density',
+    'duplicated_lines_density',
+    'coverage',
+    'ncloc',
+    'tests',
+    'test_errors',
+    'test_failures',
+    'test_execution_time',
+    'security_rating'
+];
+
+const SONAR_URL = `https://sonarcloud.io/api/measures/component_tree?component=${SONAR_ID}&metricKeys=${METRIC_LIST.join(',')}&ps=500`;
 
 module.exports = {
-  SONAR_URL,
-  REPO,
-  OWNER
-}
+    SONAR_URL,
+    REPO,
+    OWNER
+};
