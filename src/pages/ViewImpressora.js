da7686c1e52af5436e9ee4dc49687eb5e3a43970
LipeAKugel <felipearaujodff@gmail.com>
2023-10-31 00:40:01 -0300

    :bulb: (#103) estilização do card de informacoes
    Co-authored-by: Igorabreueng <igoralves.ab@gmail.com>

--- src/pages/ViewImpressora.js
+++ src/pages/ViewImpressora.js
@@ -6,17 +6,20 @@ import voltar_vector from '../assets/voltar_vector.svg'
 export default function ViewImpressora(){
     return(
         <div id="container-view">
+            <div id="left-content">
+                
+            </div>
             <div id="view-card">
                 <div id="info-group">
                     <header id="card-header">
                         <img alt="" src={voltar_vector}></img>
                         <a href="#">Voltar</a>
                     </header>
-                    <p id="cabecalho">Impressora HP Laser 107 A</p>
+                    <p id="cabecalho">Multifuncional Colorida - HP - Laser 107 A</p>
                     <div id="info-line">
                         <div id="info-box">
-                            <label>Tipo</label>
-                            <p>Multifuncional Colorida</p>
+                            <label>Número de série</label>
+                            <p>XXXX-000000</p>
                         </div>
                         <div id="info-box">
                             <label>IP</label>
@@ -25,8 +28,8 @@ export default function ViewImpressora(){
                     </div>
                     <div id="info-line">
                         <div id="info-box">
-                            <label>Número de série</label>
-                            <p>XXXX-000000</p>
+                            <label>MIB</label>
+                            <p>HP.json</p>
                         </div>
                         <div id="info-box">
                             <label>Código da locadora</label>
@@ -35,18 +38,8 @@ export default function ViewImpressora(){
                     </div>
                     <div id="info-line">
                         <div id="info-box">
-                            <label>Marca</label>
-                            <p>HP</p>
-                        </div>
-                        <div id="info-box">
-                            <label>Modelo</label>
-                            <p>Laser 107 A</p>
-                        </div>
-                    </div>
-                    <div id="info-line">
-                        <div id="info-box">
-                            <label>MIB</label>
-                            <p>HP.json</p>
+                            <label>Contador de instalação</label>
+                            <p>0</p>
                         </div>
                         <div id="info-box">
                             <label>Data de instalação</label>
@@ -55,7 +48,7 @@ export default function ViewImpressora(){
                     </div>
                     <div id="info-line">
                         <div id="info-box">
-                            <label>Contador de instalação</label>
+                            <label>Contador de retirada</label>
                             <p>0</p>
                         </div>
                         <div id="info-box">
@@ -65,15 +58,14 @@ export default function ViewImpressora(){
                     </div>
                     <div id="info-line">
                         <div id="info-box">
-                            <label>Contador de retirada</label>
-                            <p>0</p>
-                        </div>
-                        <div id="info-box">
                             <label>Unidade de localização</label>
                             <p>Catalão</p>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="elipse-view">
+                <img alt= "elipse"  src={login_ellipse}></img>
             </div>
         </div>
     );
