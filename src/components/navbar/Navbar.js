6b893f4a78d3f26ead8be57077612827bcfb831f
julisous <julia.farias2000@hotmail.com>
2023-11-01 09:30:18 -0300

    :bug: (#180) Arrumando o dropdown

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,22 +8,27 @@ const Navbar = () => {
       <img alt="" src={logo}></img>
 
       <div className="button-navbar">
-        <button className="home-navbar">Home</button>
+        <button className="home-navbar">
+          <a href="http://localhost:3000/">Home</a>
+        </button>
         <button className="aboutUs-navbar">Quem Somos</button>
         <button className="contact-navbar">Contato</button>
       </div>
 
-      <div className="dropdown-navbar">
+      <div className="dropdown-navbar-users">
         <button className="users-navbar">
           Usuários
           <div className="dropdown-users-navbar">
-            <a href="#">Cadastro de usuário</a>
+            <a href="http://localhost:3000/cadastro">Cadastro de usuário</a>
             <a href="#">Edição de usuário</a>
           </div>
         </button>
+      </div>
+
+      <div className="dropdown-navbar-printers">
         <button className="printers-navbar">
           Impressoras
-          <div className="dropdown-printer-navbar">
+          <div className="dropdown-printers-navbar">
             <a href="#">Cadastro de impressora</a>
             <a href="#">Edição de impressora</a>
           </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,39 @@
 import React from "react";
-import  "../style/pages/home.css";
+import "../style/pages/home.css";
 import Button from "../components/Button";
-import home_image from '../assets/home_image.svg';
-import elipse from '../assets/home_elipse.svg';
+import home_image from "../assets/home_image.svg";
+import elipse from "../assets/home_elipse.svg";
 import Navbar from "../components/navbar/Navbar";
 
-
-
-
-export default function Home(){
-    return(
-    
-        
-    <div className="homepage">
-        <Navbar/>
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="homepage">
         <div className="content-home">
-                <div className="left-content-home">
-                    <div className="aligned-content">
-                        <div className="page-text-home">
-                            <b className="printgo-logo">PrintGo</b>
-                            <div className="home-text">
-                                Contando impressões desde 2023
-                            </div>
-                        </div>
-                        <div className="containter-button-home">
-                            <div className="learn-more">
-                                <Button
-                                    textColor="#036"
-                                    bgColor="white"
-                                    borderColor="#036"
-                                >
-                                    Saiba mais
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="elipse-home">
-                        <img alt="" src={elipse}></img>
-                    </div>
+          <div className="left-content-home">
+            <div className="aligned-content">
+              <div className="page-text-home">
+                <b className="printgo-logo">PrintGo</b>
+                <div className="home-text">Contando impressões desde 2023</div>
+              </div>
+              <div className="containter-button-home">
+                <div className="learn-more">
+                  <Button textColor="#036" bgColor="white" borderColor="#036">
+                    Saiba mais
+                  </Button>
                 </div>
-                <div className="right-content-home">
-                    <img className="home-image" alt="" src={home_image} />
-                </div>
+              </div>
             </div>
-
+            <div className="elipse-home">
+              <img alt="" src={elipse}></img>
+            </div>
+          </div>
+          <div className="right-content-home">
+            <img className="home-image" alt="" src={home_image} />
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 }
