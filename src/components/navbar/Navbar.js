85d4f1156e7338fde13f5dc1418d17d1636a39e2
vitordiniz25 <vitordiniz25@gmail.com>
2023-11-01 13:05:16 -0300

    :bulb:(#180): adicionando icones na logica de dropdown

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Importe o Link
 import "../../style/components/navbar.css";
 import logo from "../../assets/logo 3.svg";
 
+import { FiChevronDown } from "react-icons/fi";
+
 const Navbar = () => {
+  const [userDropdownOpen, setUserDropdownOpen] = useState(false);
+  const [printerDropdownOpen, setPrinterDropdownOpen] = useState(false);
+
+  const toggleUserDropdown = () => {
+    setUserDropdownOpen(!userDropdownOpen)
+  }
+
+  const togglePrinterDropdown = () => {
+    setPrinterDropdownOpen(!printerDropdownOpen)
+  }
+
   return (
     <div className="container-navbar">
       <img alt="" src={logo}></img>
@@ -21,22 +34,26 @@ const Navbar = () => {
       </div>
 
       <div className="dropdown-navbar-users">
-        <button className="users-navbar">
-          Usuários
-          <div className="dropdown-users-navbar">
-            <Link to="/cadastro">Cadastro de usuário</Link>
-            <Link to="#">Edição de usuário</Link>
-          </div>
+        <button className="users-navbar" onClick={toggleUserDropdown}>
+          Usuários <FiChevronDown />
+          {userDropdownOpen && (
+            <div className="dropdown-users-navbar">
+              <Link to="/cadastro">Cadastro de usuário</Link>
+              <Link to="#">Edição de usuário</Link>
+            </div>
+          )}
         </button>
       </div>
 
       <div className="dropdown-navbar-printers">
-        <button className="printers-navbar">
-          Impressoras
-          <div className="dropdown-printers-navbar">
-            <Link to="#">Cadastro de impressora</Link>
-            <Link to="#">Edição de impressora</Link>
-          </div>
+        <button className="printers-navbar" onClick={togglePrinterDropdown}>
+          Impressoras <FiChevronDown />
+          {printerDropdownOpen && (
+            <div className="dropdown-printers-navbar">
+              <Link to="#">Cadastro de impressora</Link>
+              <Link to="#">Edição de impressora</Link>
+            </div>
+          )}
         </button>
       </div>
     </div>
