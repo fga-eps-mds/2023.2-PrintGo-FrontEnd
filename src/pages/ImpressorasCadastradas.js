32bc82a22ee8ce17e0cf6d9bd4519511461d4adc
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-11-01 12:51:16 -0300

    :racehorse:(#164): Mudando a disposição de elementos da página

--- src/pages/ImpressorasCadastradas.js
+++ src/pages/ImpressorasCadastradas.js
@@ -2,33 +2,23 @@ import React from "react";
 import  "../style/pages/impressorasCadastradas.css";
 import Search from '../assets/Search.svg';
 import Filter from '../assets/Filter.svg';
+import Input from '../components/Input';
 
 export default function ImpressorasCadastradas(){
     return(
         <div className="impressorasCadastradas-page">
-            <div className="impressorasCadastradas-text">
-                Impressoras cadastradas
-                <div className="impressorasCadastradas-image">
+            <div className="impressorasCadastradas-cabecalho">
+                <h2>Impressoras cadastradas</h2>
+                <div className="impressorasCadastradas-cabecalho-icones">
+                  <div className="impressorasCadastradas-cabecalho-input">
+                    <Input className="input-pesquisa-impressoras"/>
+                  </div>
+                  
                   <img alt="" src={Search} />
                   <img alt="" src={Filter} />
                   
                 </div>
 
-                {/* <div className="impressorasCadastradas-image-Search">
-                  <img alt="" src={Search} />
-                </div>
-
-                <div className="impressorasCadastradas-image-Filter">
-                  <img alt="" src={Filter} />
-                </div> */}
-
-                <div className="impressorasCadastradas-left-content">
-                    <div className="impressorasCadastradas-aligned-content">
-                        <div className="impressorasCadastradas-page-text">
-
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     );
