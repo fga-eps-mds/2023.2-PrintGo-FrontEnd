d6c201b0c89f7b35f9c790e7abc4a50e7b6b9c49
LipeAKugel <felipearaujodff@gmail.com>
2023-11-04 01:01:56 -0300

    :racehorse:(#103): Melhorando página de visualização de impressora.
    Co-authored-by: Igorabreueng <igoralves.ab@gmail.com>

--- src/pages/ViewImpressora.js
+++ src/pages/ViewImpressora.js
@@ -9,63 +9,65 @@ export default function ViewImpressora(){
             <div id="left-content">
                 
             </div>
-            <div id="view-card">
-                <div id="info-group">
-                    <header id="card-header">
-                        <img alt="" src={voltar_vector}></img>
-                        <a href="#">Voltar</a>
-                    </header>
-                    <p id="cabecalho">Multifuncional Colorida - HP - Laser 107 A</p>
-                    <div id="info-line">
-                        <div id="info-box">
-                            <label>Número de série</label>
-                            <p>XXXX-000000</p>
+            <div id="right-content">
+                <div id="view-card">
+                    <div id="info-group">
+                        <header id="card-header">
+                            <img alt="" src={voltar_vector}></img>
+                            <a href="#">Voltar</a>
+                        </header>
+                        <p id="cabecalho">Multifuncional Colorida - HP - Laser 107 A</p>
+                        <div id="info-line">
+                            <div id="info-box">
+                                <label>Número de série</label>
+                                <p>XXXX-000000</p>
+                            </div>
+                            <div id="info-box">
+                                <label>IP</label>
+                                <p>192.168.15.1</p>
+                            </div>
                         </div>
-                        <div id="info-box">
-                            <label>IP</label>
-                            <p>192.168.15.1</p>
+                        <div id="info-line">
+                            <div id="info-box">
+                                <label>MIB</label>
+                                <p>HP.json</p>
+                            </div>
+                            <div id="info-box">
+                                <label>Código da locadora</label>
+                                <p>xMnp0-A</p>
+                            </div>
                         </div>
-                    </div>
-                    <div id="info-line">
-                        <div id="info-box">
-                            <label>MIB</label>
-                            <p>HP.json</p>
+                        <div id="info-line">
+                            <div id="info-box">
+                                <label>Contador de instalação</label>
+                                <p>0</p>
+                            </div>
+                            <div id="info-box">
+                                <label>Data de instalação</label>
+                                <p>12/10/2023</p>
+                            </div>
                         </div>
-                        <div id="info-box">
-                            <label>Código da locadora</label>
-                            <p>xMnp0-A</p>
+                        <div id="info-line">
+                            <div id="info-box">
+                                <label>Contador de retirada</label>
+                                <p>0</p>
+                            </div>
+                            <div id="info-box">
+                                <label>Data de retirada</label>
+                                <p>-</p>
+                            </div>
                         </div>
-                    </div>
-                    <div id="info-line">
-                        <div id="info-box">
-                            <label>Contador de instalação</label>
-                            <p>0</p>
-                        </div>
-                        <div id="info-box">
-                            <label>Data de instalação</label>
-                            <p>12/10/2023</p>
-                        </div>
-                    </div>
-                    <div id="info-line">
-                        <div id="info-box">
-                            <label>Contador de retirada</label>
-                            <p>0</p>
-                        </div>
-                        <div id="info-box">
-                            <label>Data de retirada</label>
-                            <p>-</p>
-                        </div>
-                    </div>
-                    <div id="info-line">
-                        <div id="info-box">
-                            <label>Unidade de localização</label>
-                            <p>Catalão</p>
+                        <div id="info-line">
+                            <div id="info-box">
+                                <label>Unidade de localização</label>
+                                <p>Catalão</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="elipse-view">
-                <img alt= "elipse"  src={login_ellipse}></img>
+                <div className="elipse-view">
+                    <img alt= "elipse"  src={login_ellipse}></img>
+                </div>
             </div>
         </div>
     );
