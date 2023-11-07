0c205e86dfbf040b3a1acad7be0a3314dc29156a
mibasFerraz <lucasllff@gmail.com>
2023-11-06 23:07:14 -0300

    :bulb:(#165): Corrigindo alteracoes

--- src/pages/RegisterPrinter.js
+++ src/pages/RegisterPrinter.js
@@ -1,13 +1,13 @@
-import "../style/pages/createUser.css";
+import "../style/pages/registerPrinter.css";
 import React from "react";
-import SignupForm from "../components/forms/SignupForm";
-import signup_image from "../assets/signup_image.svg";
+import RegisterPrinterForm from "../components/forms/RegisterPrinterForm.js";
+import registerPrinter_image from "../assets/registerPrinter_image.svg";
 
-export default function CreateUserPage(){
+export default function RegisterPrinterPage(){
     return(
-        <div id="signup-container">
-            <img alt="" src={signup_image} />
-            <SignupForm/>
+        <div id="registerPrinter-container">
+            <img alt="" src={registerPrinter_image} />
+            <RegisterPrinterForm/>
         </div>
     );
 }
