73dbafde75327207d1492b301023779f328752bb
Wolffstein <wolffriederichstein@gmail.com>
2023-11-07 21:50:38 -0300

    :repeat: (#164): Adicionando containers para resultado de pesquisa
    Co-authored-by: Eduardo de Melo Colli <eduardodemelocolli@gmail.com >

--- src/pages/ImpressorasCadastradas.js
+++ src/pages/ImpressorasCadastradas.js
@@ -5,20 +5,29 @@ import Filter from '../assets/Filter.svg';
 import Input from '../components/Input';
 
 export default function ImpressorasCadastradas(){
-    return(
-        <div className="impressorasCadastradas-page">
-            <div className="impressorasCadastradas-cabecalho">
-                <h2>Impressoras cadastradas</h2>
-                <div className="impressorasCadastradas-cabecalho-icones">
-                  <div className="impressorasCadastradas-cabecalho-input">
-                    <Input className="input-pesquisa-impressoras"/>
-                  </div>
-                  <div className="impressorasCadastradas-cabecalho-imagens"> 
-                    <img alt="" src={Search} />
-                    <img alt="" src={Filter} />
-                  </div>
-                </div>
-            </div>
+  return(
+    <div className="impressorasCadastradas-page">
+      <div className="impressorasCadastradas-cabecalho">
+        <h2>Impressoras cadastradas</h2>
+        <div className="impressorasCadastradas-cabecalho-icones">
+          <div className="impressorasCadastradas-cabecalho-input">
+            <Input className="input-pesquisa-impressoras"/>
+          </div>
+          <div className="impressorasCadastradas-cabecalho-imagens"> 
+            <img alt="" src={Search} />
+            <img alt="" src={Filter} />
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="impressorasCadastradas-Lista">
+        <h6>Impressora HP Laser 107 A - - - Catalão-GO - - - 9°DRP - 2°DM - - - img - img</h6>
+        <h6>Impressora HP InkJet 50 - - -  - -Goiânia-GO - - - 1°DRP - 9°DM - - - img - img</h6>
+        <h6>Impressora Epson EcoTank - - - -Formosa-GO - - 11°DRP -5°DM - - - img - img</h6>
+          <div className="impressorasCadastradas-tester">
+              <h7>olaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h7>
+             </div>
+      </div>
+    </div>
+  );
 }
