a31403accb579d7041bafa980eed8fe0fc81e386
vitordiniz25 <vitordiniz25@gmail.com>
2023-11-09 18:31:33 -0300

    :test:(#180): adicionando teste para mensagem de erro para confirmação de senha vazia

--- src/tests/pages/ChangePassword.test.js
+++ src/tests/pages/ChangePassword.test.js
@@ -71,4 +71,22 @@ describe('Change Password page', () => {
             expect(screen.queryByText('Senha inválida'))
         });
     });
+
+    it('should display error for empty password confirmation', async () => {
+        render(<ChangePassword />);
+  
+        const newPasswordInput = screen.getByPlaceholderText('*****************');
+        const repeatPasswordInput = screen.getByPlaceholderText('******************');
+        const submitButton = screen.getByText('Confirmar');
+
+        fireEvent.change(newPasswordInput, { target: { value: 'novaSenha123' } });
+        fireEvent.change(repeatPasswordInput, { target: { value: '' } });
+
+        fireEvent.click(submitButton);
+
+        // Aguarda a resposta assíncrona (pode ser ajustado conforme necessário)
+        await waitFor(() => {
+            expect(screen.queryByText('Confirmação de Senha inválida'))
+        });
+    });
 })
