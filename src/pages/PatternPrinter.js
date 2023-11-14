ca23c0e39f21167f3d559d73c95ff4f9d1bc7906
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-11-14 12:48:00 -0300

    :repeat:(#165): Fazendo alterações no estilo da página de cadastro de padrões

--- src/pages/PatternPrinter.js
+++ src/pages/PatternPrinter.js
@@ -5,7 +5,7 @@ import registerPrinter_image from "../assets/registerPrinter_image.svg";
 
 export default function PatternPrinter(){
     return(
-        <div id="registerPrinter-container">
+        <div id="register-printer-pattern-container">
             <img alt="" src={registerPrinter_image} />
             <PrinterPatternForm/>
         </div>
