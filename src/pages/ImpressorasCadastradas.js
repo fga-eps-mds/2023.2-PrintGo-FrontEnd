db935ed59260e43bc3293f465dcdb30f9aaf0727
LipeAKugel <felipearaujodff@gmail.com>
2023-11-15 00:25:39 -0300

    :racehorse: (#164) Adicionando pesquisas, visualização de impressora desativada e desativação ou ativação de impressora
    Co-authored-by: WolffStein <Wolffriederichstein@gmail.com>
    Co-authored-by: AntonioRangelC <antonio.rangel.02@gmail.com>

--- src/pages/ImpressorasCadastradas.js
+++ src/pages/ImpressorasCadastradas.js
@@ -10,36 +10,42 @@ import { useState } from "react";
 
 
 export default function ImpressorasCadastradas(){
-   
-  const impressoras = [
-    {
-      modelo: 'HP InkJet 50',
-      padrao: 'HPInkJet-1234-ABCD',
-      ip: '987.654.32',
-      codigo_loc: 'PRINTER-002',
-      unidade_pai: '1°DRP',
-      unidade_filha: '2ª DM',
-      contador: '83',
-      data: '07/11/23',
-      imagem: 'caminho/para/imagem1.jpg',
-    },
-    {
-      modelo: 'Epson LaserJet',
-      padrao: 'EpsonLaser-5678-EFGH',
-      ip: '340.90.98',
-      codigo_loc: 'PRINTER-003',
-      unidade_pai: '2°DRP',
-      unidade_filha: '3ª DM',
-      contador: '88',
-      data: '08/11/23',
-      imagem: 'caminho/para/imagem2.jpg',
-    },
-    
-  ];
+  
+  const [impressoras, setImpressoras] = useState(
+    [
+      {
+        modelo: 'HP InkJet 50',
+        numeroSerie: 'HPInkJet-1234-ABCD',
+        ip: '987.654.32',
+        codigo_loc: 'PRINTER-002',
+        unidade_pai: '1°DRP',
+        unidade_filha: '2ª DM',
+        contador: '83',
+        data: '07/11/23',
+        ativada: false,
+        imagem: 'caminho/para/imagem1.jpg', 
+      },
+      {
+        modelo: 'Epson LaserJet',
+        numeroSerie: 'EpsonLaser-5678-EFGH',
+        ip: '340.90.98',
+        codigo_loc: 'PRINTER-003',
+        unidade_pai: '2°DRP',
+        unidade_filha: '3ª DM',
+        contador: '88',
+        data: '08/11/23',
+        ativada: true,
+        imagem: 'caminho/para/imagem2.jpg',
+      },
+  ]
+  )
 
-  const [dropdown, setDropdown] = useState(false);
+  const [dropdown, setDropdown] = useState(true);
   const [search, setSearch] = useState('');
   const [modalOpen, setModalOpen] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalBodytext, setModalBodytext] = useState('');
+  const [selectedPrinterID, setSelectedPrinterID] = useState();
 
   console.log(search)
 
@@ -47,16 +53,36 @@ export default function ImpressorasCadastradas(){
     setDropdown(!dropdown)
   };
 
+  const deactivatePrinter = (printerID) => {
+    setSelectedPrinterID(printerID);
+    setModalTitle("Desativação de impressora");
+    setModalBodytext("Você tem certeza que deseja desativar a impressora?");
+  }
+
+  const activePrinter = (printerID) => {
+    setSelectedPrinterID(printerID);
+    setModalTitle("Ativação de impressora");
+    setModalBodytext("Você tem certeza que deseja reativar a impressora?");
+  }
+
+  const printerToggle = () => {
+    const updatedImpressoras = [...impressoras];
+    updatedImpressoras[selectedPrinterID].ativada = !updatedImpressoras[selectedPrinterID].ativada;
+    setImpressoras(updatedImpressoras);
+    setModalOpen(false);
+  };
 
   return(
     <div className="impressorasCadastradas-page">
       {modalOpen && (
         <Modal 
           setOpenModal={setModalOpen} 
-          title={"Desativação de impressora"} 
-          bodytext={"Você tem certeza que deseja desativar esta impressora?"}
+          title={modalTitle} 
+          bodytext={modalBodytext}
+          onConfirm={printerToggle}
         />
         
+
       )}
       <div className="impressorasCadastradas-cabecalho">
         <h2>Impressoras cadastradas</h2>
@@ -78,29 +104,36 @@ export default function ImpressorasCadastradas(){
       {impressoras.filter((impressora) => {
         return search.toLowerCase() === '' 
         ? impressora 
-        : impressora.nome.toLowerCase().includes(search)
+        : impressora.codigo_loc.toLocaleLowerCase().includes(search) ||
+          impressora.unidade_pai.toLocaleLowerCase().includes(search) ||
+          impressora.unidade_filha.toLocaleLowerCase().includes(search) ||
+          impressora.ip.toLocaleLowerCase().includes(search) ||
+          impressora.modelo.toLocaleLowerCase().includes(search) ||
+          impressora.numeroSerie.toLocaleLowerCase().includes(search)
+
       
       
       }).map((impressora, index) => (
 
-        <div key={index} className="impressorasCadastradas-Lista">
+        <div key={index} className="impressorasCadastradas-Lista" style={{ color: impressora.ativada ? '' : 'gray' }}>
           <div className="impressoras-modelo">
             <h4>{impressora.modelo}</h4>
+            {!impressora.ativada && (<h5>Desativada</h5>)}
           </div>
           
-          <div className="impressoras-identificacao"> 
-            <h6>{impressora.padrao}</h6>
+          <div className="impressoras-identificacao" style={{ color: impressora.ativada ? '' : 'gray' }}> 
+            <h6>{impressora.numeroSerie}</h6>
             <h6>IP: {impressora.ip}</h6>
             <h6>{impressora.codigo_loc}</h6> 
           </div>
           
-          <div className="impressoras-unidade-contador">
+          <div className="impressoras-unidade-contador" style={{ color: impressora.ativada ? '' : 'gray' }}>
             <h6>{impressora.unidade_pai}</h6>
             <h6>{impressora.unidade_filha}</h6>
             <h6>{impressora.contador}</h6>
           </div>
           
-          <div className="data-ultimo-contador">
+          <div className="data-ultimo-contador" style={{ color: impressora.ativada ? '' : 'gray' }}>
             <h6>Data do último contador: {impressora.data}</h6>
           </div>
           
@@ -109,13 +142,24 @@ export default function ImpressorasCadastradas(){
             <img 
               alt="" 
               src={engine}
-              onClick={toggleDropdown}
             />
             <div className="impressorasCadastradas-dropdown-container">
               {dropdown && (
                 <div className="impressorasCadastradas-dropdown">
                   <div onClick={() => {setModalOpen(true);}}>
-                    <Link to="#">Desativar</Link>
+                    {
+                      (impressora.ativada && 
+                        ( <Link to="#" onClick={() => {deactivatePrinter(index)}}>
+                            Desativar
+                          </Link>
+                        )
+                      ) || (!impressora.ativada && 
+                        ( <Link to="#" onClick={() => {activePrinter(index)}}>
+                            Ativar
+                          </Link>
+                        )
+                      ) 
+                    }
                   </div>
                   <Link to="#">Editar</Link>
                 </div>
@@ -130,39 +174,3 @@ export default function ImpressorasCadastradas(){
     </div>
   );
 }
-
-
-/*
-
-// SeuComponentePrincipal.js
-import React, { useState } from 'react';
-import Modal from './Modal';
-
-const SeuComponentePrincipal = () => {
-  const [mostrarModal, setMostrarModal] = useState(false);
-  const [textoDoModal, setTextoDoModal] = useState('');
-
-  const abrirModal = (texto) => {
-    setTextoDoModal(texto);
-    setMostrarModal(true);
-  };
-
-  const fecharModal = () => {
-    setMostrarModal(false);
-    setTextoDoModal('');
-  };
-
-  return (
-    <div>
-      <button onClick={() => abrirModal('Seu texto aqui')}>Abrir Modal</button>
-
-      {mostrarModal && (
-        <Modal texto={textoDoModal} onClose={fecharModal} />
-      )}
-    </div>
-  );
-};
-
-export default SeuComponentePrincipal;
-
-*/
