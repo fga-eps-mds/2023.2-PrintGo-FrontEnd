5436945d56c459439e8ea2a5c316bfb3292e6e87
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-11-16 08:46:13 -0300

    :racehorse:(#164): Padronizando caminho

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Route path="/contato" element={<Contact />} />
         <Route path="/quemsomos" element={<AboutUs />} />
         <Route path="/mudarsenha" element={<ChangePassword />} />
-        <Route path="/printersList" element={<PrintersList/>}/>
+        <Route path="/impressorascadastradas" element={<PrintersList/>}/>
     </Routes>
     </BrowserRouter>
   );
