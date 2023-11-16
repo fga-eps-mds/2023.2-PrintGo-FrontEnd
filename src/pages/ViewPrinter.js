35087fdda3946806e5021821b515d34117be5a0d
LipeAKugel <felipearaujodff@gmail.com>
2023-11-16 15:14:10 -0300

    :bulb: (#103) Adicionando Navbar

--- src/pages/ViewPrinter.js
+++ src/pages/ViewPrinter.js
@@ -3,6 +3,7 @@ import { getPrinterData } from "../api/api";
 import '../style/pages/viewPrinter.css';
 import login_ellipse from '../assets/login_ellipse.svg';
 import voltar_vector from '../assets/voltar_vector.svg';
+import Navbar from "../components/navbar/Navbar";
 
 export default function ViewImpressora(){
     // Estado para armazenar os dados da impressora.
@@ -37,78 +38,81 @@ export default function ViewImpressora(){
     }
 
     return(
-        <div id="container-viewprinter">
-            <div id="viewprinter-left-content"></div>
-            <div id="viewprinter-right-content">
-                <div id="viewprinter-card">
-                    <div id="viewprinter-info-group">
-                        <header id="viewprinter-card-header">
-                            <img alt="" src={voltar_vector}></img>
-                            <a href="">Voltar</a>
-                        </header>
-                        <p id="viewprinter-info-header">Multifuncional P&B - Canon - MF1643i II</p>
-                        <div id="viewprinter-info-line">
-                            <div id="viewprinter-info-box">
-                                <label>Número de série</label>
-                                <p>{exampleData && exampleData.numeroSerie}</p>
+        <>
+            <Navbar />
+            <div id="container-viewprinter">
+                <div id="viewprinter-left-content"></div>
+                <div id="viewprinter-right-content">
+                    <div id="viewprinter-card">
+                        <div id="viewprinter-info-group">
+                            <header id="viewprinter-card-header">
+                                <img alt="" src={voltar_vector}></img>
+                                <a href="">Voltar</a>
+                            </header>
+                            <p id="viewprinter-info-header">Multifuncional P&B - Canon - MF1643i II</p>
+                            <div id="viewprinter-info-line">
+                                <div id="viewprinter-info-box">
+                                    <label>Número de série</label>
+                                    <p>{exampleData && exampleData.numeroSerie}</p>
+                                </div>
+                                <div id="viewprinter-info-box">
+                                    <label>IP</label>
+                                    <p>{exampleData && exampleData.ip}</p>
+                                </div>
                             </div>
-                            <div id="viewprinter-info-box">
-                                <label>IP</label>
-                                <p>{exampleData && exampleData.ip}</p>
+                            <div id="viewprinter-info-line">
+                                <div id="viewprinter-info-box">
+                                    <label>Código da locadora</label>
+                                    <p>{exampleData && exampleData.codigoLocadora}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div id="viewprinter-info-line">
-                            <div id="viewprinter-info-box">
-                                <label>Código da locadora</label>
-                                <p>{exampleData && exampleData.codigoLocadora}</p>
+                            <div id="viewprinter-info-line">
+                                <div id="viewprinter-info-box">
+                                    <label>Contador de instalação</label>
+                                    <p>{exampleData && exampleData.contadorInstalacao}</p>
+                                </div>
+                                <div id="viewprinter-info-box">
+                                    <label>Data de instalação</label>
+                                    <p>{exampleData && exampleData.dataInstalacao}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div id="viewprinter-info-line">
-                            <div id="viewprinter-info-box">
-                                <label>Contador de instalação</label>
-                                <p>{exampleData && exampleData.contadorInstalacao}</p>
+                            <div id="viewprinter-info-line">
+                                <div id="viewprinter-info-box">
+                                    <label>Contador de retirada</label>
+                                    <p>{exampleData && exampleData.contadorRetirada}</p>
+                                </div>
+                                <div id="viewprinter-info-box">
+                                    <label>Data de retirada</label>
+                                    <p>{exampleData && exampleData.dataRetirada}</p>
+                                </div>
                             </div>
-                            <div id="viewprinter-info-box">
-                                <label>Data de instalação</label>
-                                <p>{exampleData && exampleData.dataInstalacao}</p>
+                            <div id="viewprinter-info-line">
+                                <div id="viewprinter-info-box">
+                                    <label>Último contador</label>
+                                    <p>{exampleData && exampleData.ultimoContador}</p>
+                                </div>
+                                <div id="viewprinter-info-box">
+                                    <label>Data do último contador</label>
+                                    <p>{exampleData && exampleData.dataUltimoContador}</p>
+                                </div>
                             </div>
-                        </div>
-                        <div id="viewprinter-info-line">
-                            <div id="viewprinter-info-box">
-                                <label>Contador de retirada</label>
-                                <p>{exampleData && exampleData.contadorRetirada}</p>
-                            </div>
-                            <div id="viewprinter-info-box">
-                                <label>Data de retirada</label>
-                                <p>{exampleData && exampleData.dataRetirada}</p>
-                            </div>
-                        </div>
-                        <div id="viewprinter-info-line">
-                            <div id="viewprinter-info-box">
-                                <label>Último contador</label>
-                                <p>{exampleData && exampleData.ultimoContador}</p>
-                            </div>
-                            <div id="viewprinter-info-box">
-                                <label>Data do último contador</label>
-                                <p>{exampleData && exampleData.dataUltimoContador}</p>
-                            </div>
-                        </div>
-                        <div id="viewprinter-info-line">
-                            <div id="viewprinter-info-box">
-                                <label>Unidade pai</label>
-                                <p>{exampleData && exampleData.unidadePai}</p>
-                            </div>
-                            <div id="viewprinter-info-box">
-                                <label>Unidade filha</label>
-                                <p>{exampleData && exampleData.unidadeFilha}</p>
+                            <div id="viewprinter-info-line">
+                                <div id="viewprinter-info-box">
+                                    <label>Unidade pai</label>
+                                    <p>{exampleData && exampleData.unidadePai}</p>
+                                </div>
+                                <div id="viewprinter-info-box">
+                                    <label>Unidade filha</label>
+                                    <p>{exampleData && exampleData.unidadeFilha}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="elipse-viewprinter">
-                    <img alt= "elipse"  src={login_ellipse}></img>
+                    <div className="elipse-viewprinter">
+                        <img alt= "elipse"  src={login_ellipse}></img>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
