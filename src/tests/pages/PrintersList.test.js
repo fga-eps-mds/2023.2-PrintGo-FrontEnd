acff91b8f26eca3d509f89da2277bf6baae5c0d2
julisous <julia.farias2000@hotmail.com>
2023-11-16 15:59:45 -0300

    :bulb: (#164) Criação de teste para filtro de pesquisa

--- src/tests/pages/PrintersList.test.js
+++ src/tests/pages/PrintersList.test.js
@@ -7,3 +7,13 @@ test("deve renderizar a lista de impressoras corretamente", () => {
   render(<ImpressorasCadastradas />);
   expect(screen.getByText("Impressoras cadastradas")).toBeInTheDocument();
 });
+
+// Teste de Filtro de Pesquisa
+test("deve filtrar impressoras com base na pesquisa", () => {
+  render(<ImpressorasCadastradas />);
+  fireEvent.change(screen.getByRole("textbox"), {
+    target: { value: "HP InkJet 50" },
+  });
+  expect(screen.getByText("HP InkJet 50")).toBeInTheDocument();
+  expect(screen.queryByText("Epson LaserJet")).toBeNull();
+});
