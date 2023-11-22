531b1dcb924e2e40c3d7b71a98242019419ffdc7
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-11-21 23:12:07 -0300

    :repeat:(#164): Aumentando quantidade de testes

--- src/tests/pages/PrintersList.test.js
+++ src/tests/pages/PrintersList.test.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import ImpressorasCadastradas from "../../pages/PrintersList";
+import { render, screen, fireEvent, waitFor} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import PrintersList from "../../pages/PrintersList";
 import * as api from "../../api/api";
 import * as router from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 // Mock das chamadas de API
 jest.mock("../../api/api", () => ({
@@ -29,14 +31,18 @@ beforeEach(() => {
 
 // Teste de Renderização Inicial
 test("deve renderizar a lista de impressoras corretamente", async () => {
-  render(<ImpressorasCadastradas />);
+  render(<BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>);
   expect(await screen.findByText("HP InkJet 50")).toBeInTheDocument();
   expect(await screen.findByText("Epson LaserJet")).toBeInTheDocument();
 });
 
 // Teste de Filtro de Pesquisa
 test("deve filtrar impressoras com base na pesquisa", () => {
-  render(<ImpressorasCadastradas />);
+  render(<BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>);
   fireEvent.change(screen.getByRole("textbox"), {
     target: { value: "HP InkJet 50" },
   });
@@ -44,31 +50,46 @@ test("deve filtrar impressoras com base na pesquisa", () => {
   expect(screen.queryByText("Epson LaserJet")).toBeNull();
 });
 
-//Teste de Atualização de Status com Mock da API
-test("deve atualizar o status da impressora", async () => {
-  render(<ImpressorasCadastradas />);
 
-  const botaoDesativar = await screen.findByText("Desativar", {
-    selector: "button",
-  });
+test("deve mostrar se a impressora esta sendo desativada", async () => {
+  render(<BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>);
+
+  
+  const botaoDesativar = screen.getByText("Desativar");
   fireEvent.click(botaoDesativar);
 
-  await waitFor(() => {
-    expect(api.updatePrinterStatus).toHaveBeenCalledWith();
-  });
-
-  expect(await screen.findByText("Desativada")).toBeInTheDocument();
+ 
+  expect(screen.getByText("Desativação de impressora")).toBeInTheDocument();
 });
+
+test("deve mostrar se a impressora esta sendo ativada", async () => {
+  render(<BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>);
+
+  
+  const botaoAtivar = screen.getByText("Ativar");
+  fireEvent.click(botaoAtivar);
+
+ 
+  expect(screen.getByText("Ativação de impressora")).toBeInTheDocument();
+});
+
+
 // Teste de Modal de Ativação/Desativação
 test("deve abrir o modal de ativação/desativação e realizar a ação", async () => {
-  render(<ImpressorasCadastradas />);
+  render(<BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>);
 
   // Encontra e clica no botão que abre o modal de desativação/ativação
   const botaoDesativar = screen.getByText("Desativar");
   fireEvent.click(botaoDesativar);
 
   // Verifica se o modal foi aberto
-  const tituloModal = await screen.findByText("Desativação de impressora");
+  const tituloModal = await screen.getByText("Desativação de impressora");
   expect(tituloModal).toBeInTheDocument();
 
   // Simula clique no botão de confirmação no modal
@@ -82,10 +103,60 @@ test("deve abrir o modal de ativação/desativação e realizar a ação", async
 
 //Teste de Navegação com Mock do Router
 test("deve redirecionar para a página de detalhes da impressora", async () => {
-  const { getByText } = render(<ImpressorasCadastradas />);
+  const { getByText } = render(
+  <BrowserRouter>
+    <PrintersList />
+  </BrowserRouter>
+  );
   fireEvent.click(getByText("Ver detalhes"));
 
   expect(router.useHistory().push).toHaveBeenCalledWith(
     "/detalhes-da-impressora"
   );
 });
+
+test("Deve mostrar o texto do filto 'Ativas'", async () =>{
+
+  render(
+    <BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>
+  )
+  
+  const botaoAtivas = screen.getByText('Ativas');
+  fireEvent.click(botaoAtivas);
+  const filter_being_shown = screen.getByTestId("filter_beign_shown")
+  expect(filter_being_shown).toHaveTextContent("Ativas");
+
+})
+
+
+test("Deve mostrar o texto do filtro 'Todas'", async () =>{
+
+  render(
+    <BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>
+  )
+  
+  const botaoTodas = screen.getByText('Todas', { selector: 'a' });
+  fireEvent.click(botaoTodas);
+  const filter_being_shown = screen.getByTestId("filter_beign_shown")
+  expect(filter_being_shown).toHaveTextContent("Todas");
+
+})
+
+test("Deve mostrar o texto do filtro 'Desativadas'", async () =>{
+
+  render(
+    <BrowserRouter>
+      <PrintersList />
+    </BrowserRouter>
+  )
+  
+  const botaoDesativadas = screen.getByText('Desativadas', { selector: 'a' });
+  fireEvent.click(botaoDesativadas);
+  const filter_being_shown = screen.getByTestId("filter_beign_shown")
+  expect(filter_being_shown).toHaveTextContent("Desativadas");
+
+})
