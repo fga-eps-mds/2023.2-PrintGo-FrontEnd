5383bcdc2cd0995034bea73cd6c26726a592eb5d
Yuri Alves <yurivalerio12@gmail.com>
2023-11-21 23:45:12 -0300

    (#165) Teste para o submit form

--- src/tests/components/RegisterPrinterForm.test.js
+++ src/tests/components/RegisterPrinterForm.test.js
@@ -3,10 +3,15 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import RegisterPrinterForm, { fieldLabels } from '../../components/forms/RegisterPrinterForm';
+import { createUser } from '../../api/api'; 
 
 jest.mock('axios', () => ({
   // Sua implementação mock, se necessário
 }));
+
+jest.mock('../../api/api', () => ({
+    createUser: jest.fn(), // Mock createUser function
+  }));
 
 describe('RegisterPrinterForm', () => {
   beforeEach(() => {
@@ -57,8 +62,39 @@ describe('RegisterPrinterForm', () => {
         }
       }
     });
-});
+  });
 
-  
-  
+  test('submits form data and resets form on successful submission', async () => {
+    render(<RegisterPrinterForm />);
+    
+    // Mock console.log
+    const consoleSpy = jest.spyOn(console, 'log');
+    
+    // Fill in the form fields
+    const formData = {
+      padrao: 'Teste',
+      ip: '192.168.0.1',
+      numeroSerie: '1A2B3C4E',
+      codigoLocadora: '001',
+      contadorInstalacao: '003',
+      dataInstalacao: '11-02-2021',
+      contadorRetirada: '006',
+      dataRetirada: '11-12-2022',
+    };
+
+    Object.keys(formData).forEach(field => {
+      const input = screen.getByPlaceholderText(fieldLabels[field].includes("data") ? "DD/MM/AAAA" : fieldLabels[field]);
+      fireEvent.change(input, { target: { value: formData[field] } });
+    });
+
+    const submitButton = screen.getByRole('button', { name: /registrar/i });
+    fireEvent.click(submitButton);
+
+    // Verify createUser was called with the correct data
+    await waitFor(() => {
+      expect(createUser).toHaveBeenCalledWith(formData);
+    });
+
+    consoleSpy.mockRestore();
+  });
 });
