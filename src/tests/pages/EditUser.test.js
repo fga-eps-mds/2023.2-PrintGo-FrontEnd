139804c863fe6b47fe1315ac18172717924cea8c
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-11-28 20:28:07 -0300

    :bulb:(#193): Adicionando teste de renderização
    Co-authored-by: WolffStein <Wolffriederichstein@gmail.com>

--- src/tests/pages/EditUser.test.js
+++ src/tests/pages/EditUser.test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import EditUserPage from '../../pages/EditUser';
+import EditUserForm from '../../components/forms/EditUserForm';
+import { BrowserRouter } from 'react-router-dom';
+
 
 
 // Optionally, you can mock child components if their internal functionality is not relevant for the current test
-jest.mock("../components/navbar/Navbar", () => (props) => <div data-testid="navbar-mock">Navbar</div>);
-jest.mock("../components/forms/EditUserForm", () => (props) => <div data-testid="edit-user-form-mock">EditUserForm</div>);
+jest.mock("../../components/navbar/Navbar", () => (props) => <div data-testid="navbar-mock">Navbar</div>);
+jest.mock("../../components/forms/EditUserForm", () => (props) => <div data-testid="edit-user-form-mock">EditUserForm</div>);
 
 describe('EditUserPage Tests', () => {
   test('renders EditUserPage with all components', () => {
@@ -22,4 +25,16 @@ describe('EditUserPage Tests', () => {
     expect(screen.getByRole('img')).toHaveAttribute('src', 'signup_image.svg');
   });
 
+});
+
+test('EditUserForm renders correctly', () => {
+  const wrapper = render(
+    <BrowserRouter>
+      <EditUserPage>
+        <EditUserForm />
+      </EditUserPage>
+    </BrowserRouter>
+    
+    );
+  expect(wrapper.baseElement).toBeInTheDocument()
 });
