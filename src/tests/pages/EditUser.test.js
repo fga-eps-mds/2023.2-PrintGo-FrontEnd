819e4fa83e3081c5387c644a912d497a1864190b
Júlia Farias <julia.farias840@gmail.com>
2023-11-28 17:47:14 -0300

    :bulb: (#193) Adicionando testes para a página de edição de usuário

---
+++ src/tests/pages/EditUser.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import EditUserPage from '../../pages/EditUser';
+
+
+// Optionally, you can mock child components if their internal functionality is not relevant for the current test
+jest.mock("../components/navbar/Navbar", () => (props) => <div data-testid="navbar-mock">Navbar</div>);
+jest.mock("../components/forms/EditUserForm", () => (props) => <div data-testid="edit-user-form-mock">EditUserForm</div>);
+
+describe('EditUserPage Tests', () => {
+  test('renders EditUserPage with all components', () => {
+    render(<EditUserPage />);
+
+    // Check if Navbar is rendered
+    expect(screen.getByTestId('navbar-mock')).toBeInTheDocument();
+
+    // Check if EditUserForm is rendered
+    expect(screen.getByTestId('edit-user-form-mock')).toBeInTheDocument();
+
+    // Check if the image is rendered
+    expect(screen.getByRole('img')).toHaveAttribute('src', 'signup_image.svg');
+  });
+
+});
