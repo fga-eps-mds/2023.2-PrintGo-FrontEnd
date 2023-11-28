65dea2bc1675f87edbbab0e3e366d8f4e2f8a08f
Júlia Farias <julia.farias840@gmail.com>
2023-11-28 17:05:47 -0300

    :bulb: (#193) Adição de novos testes

--- src/tests/components/EditUserForm.test.js
+++ src/tests/components/EditUserForm.test.js
@@ -12,12 +12,11 @@ jest.mock("../../services/unidadeService", () => ({
 
 describe('EditUserForm Tests', () => {
   beforeEach(() => {
-    // Mocking getUnidades response
     getUnidades.mockResolvedValue({
       type: 'success',
       data: [
-        { id: '1', name: 'Unidade 1' },
-        { id: '2', name: 'Unidade 2' }
+        { id: '1', name: 'Unidade 1', child_workstations: [{ id: '3', name: 'Unidade 3' }] },
+        { id: '2', name: 'Unidade 2', child_workstations: [] }
       ]
     });
   });
@@ -29,7 +28,6 @@ describe('EditUserForm Tests', () => {
 
   test('submits form with valid data', async () => {
     createUser.mockResolvedValue({ type: 'success' });
-
     render(<EditUserForm />);
 
     fireEvent.change(screen.getByLabelText('Nome'), { target: { value: 'Test User' } });
@@ -47,6 +45,53 @@ describe('EditUserForm Tests', () => {
     });
   });
 
-  // Add more tests for validation, error handling, etc.
-});
+  test('displays validation errors for empty required fields', async () => {
+    render(<EditUserForm />);
+    fireEvent.click(screen.getByText('REGISTRAR'));
 
+    await waitFor(() => {
+      expect(screen.getByText('Nome é obrigatório')).toBeInTheDocument();
+      expect(screen.getByText('Email é obrigatório')).toBeInTheDocument();
+      // Add more assertions for other fields
+    });
+  });
+
+  test('tests dropdown interactions', async () => {
+    render(<EditUserForm />);
+
+    // Select a unit
+    fireEvent.change(screen.getByLabelText('Selecione Unidade Pai'), {
+      target: { value: '1' },
+    });
+
+    // Check if child workstations are displayed
+    await waitFor(() => {
+      expect(screen.getByLabelText('Selecione Unidade Filho')).toHaveOption('3', 'Unidade 3');
+    });
+  });
+
+  test('handles API error on unit fetch', async () => {
+    getUnidades.mockRejectedValue(new Error('API error'));
+    render(<EditUserForm />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Erro ao obter opções do serviço:')).toBeInTheDocument();
+    });
+  });
+
+  test('handles form submission with API error', async () => {
+    createUser.mockRejectedValue(new Error('API submission error'));
+    render(<EditUserForm />);
+
+    // Fill in the form and submit
+    fireEvent.change(screen.getByLabelText('Nome'), { target: { value: 'Test User' } });
+    fireEvent.change(screen.getByLabelText('E-mail'), { target: { value: 'test@example.com' } });
+    fireEvent.click(screen.getByText('REGISTRAR'));
+
+    await waitFor(() => {
+      expect(screen.getByText('Erro ao cadastrar usuario')).toBeInTheDocument();
+    });
+  });
+
+  // Add more tests for other scenarios and edge cases
+});
