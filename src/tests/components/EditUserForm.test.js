15a12cd5b54d5445a840c0b202dc617723849e9b
Júlia Farias <julia.farias840@gmail.com>
2023-11-28 21:51:35 -0300

    :bug: (#193) Refazendo testes

--- src/tests/components/EditUserForm.test.js
+++ src/tests/components/EditUserForm.test.js
@@ -65,6 +65,89 @@ describe('EditUserForm', () => {
         });
     });
 
-    
+    it('renders select or input based on key value', () => {
+      render(<EditUserForm />);
+      // Teste para linhas 53: verificar se os selects para 'unidadePai' e 'unidadeFilha' são renderizados
+      expect(screen.getByLabelText('Selecione Unidade Pai')).toBeInTheDocument();
+      expect(screen.getByLabelText('Selecione Unidade Filho')).toBeInTheDocument();
+  });
+
+  it('updates state on unidadePai selection', async () => {
+      render(<EditUserForm />);
+      // Teste para linhas 60 a 62: selecionar uma unidade pai e verificar a mudança de estado
+      fireEvent.change(screen.getByLabelText('Selecione Unidade Pai'), { target: { value: 'some-unit-id' } });
+      // Verificar a atualização correspondente no estado do componente...
+  });
+
+  it('shows error messages for invalid inputs', async () => {
+      render(<EditUserForm />);
+      // Teste para linha 70: verificar a renderização de mensagens de erro para campos inválidos
+      userEvent.click(screen.getByText('REGISTRAR'));
+      await waitFor(() => {
+          expect(screen.getByText('Nome é obrigatório')).toBeInTheDocument();
+          // Verificar outras mensagens de erro...
+      });
+  });
+
+  it('renders input boxes', () => {
+      render(<EditUserForm />);
+      // Teste para linha 71: verificar se as caixas de entrada são renderizadas
+      expect(screen.getByTestId('input-box')).toBeInTheDocument();
+  });
+
+  it('renders buttons with correct text', () => {
+      render(<EditUserForm />);
+      // Teste para linhas 74 e 75: verificar a renderização dos botões
+      expect(screen.getByText('CANCELAR')).toBeInTheDocument();
+      expect(screen.getByText('REGISTRAR')).toBeInTheDocument();
+  });
+
+  it('changes register button text on submitting', async () => {
+      render(<EditUserForm />);
+      // Teste para linhas 76,77,78: verificar mudança de texto no botão durante a submissão
+      userEvent.click(screen.getByText('REGISTRAR'));
+      await waitFor(() => {
+          expect(screen.getByText('CADASTRANDO')).toBeInTheDocument();
+          // Verificar se o ícone de carregamento está presente...
+      });
+  });
+
+  it('renders elipse image', () => {
+      render(<EditUserForm />);
+      // Teste para linha 82: verificar se a imagem da elipse é renderizada
+      expect(screen.getByAltText('elipse')).toBeInTheDocument();
+  });
+
+  it('displays toast messages on form submission', async () => {
+      render(<EditUserForm />);
+      // Teste para linhas 88,89 e 90: verificar exibição de mensagens toast após submissão
+      userEvent.type(screen.getByLabelText('Nome'), 'João da Silva');
+      userEvent.click(screen.getByText('REGISTRAR'));
+      await waitFor(() => {
+          expect(screen.getByText('Usuario cadastrado com sucesso!')).toBeInTheDocument();
+          // Verificar também o toast de erro...
+      });
+  });
+
+  it('resets form on successful submission', async () => {
+      render(<EditUserForm />);
+      // Teste para linha 96: verificar se o formulário é resetado após submissão bem-sucedida
+      userEvent.type(screen.getByLabelText('Nome'), 'João da Silva');
+      userEvent.click(screen.getByText('REGISTRAR'));
+      await waitFor(() => {
+          // Verificar se o formulário foi resetado...
+      });
+  });
+
+  it('handles errors in data fetching', async () => {
+      mockGetUnidades.mockRejectedValue(new Error('Erro ao obter opções do serviço'));
+      render(<EditUserForm />);
+      // Teste para linhas 98 e 99: verificar o tratamento de erros na busca de dados
+      await waitFor(() => {
+          // Verificar se o componente lida corretamente com o erro...
+      });
+  });
+
+
 });
 
