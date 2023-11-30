308b93be3c230c597643a2ae62185b44a070693c
LipeAKugel <felipearaujodff@gmail.com>
2023-11-30 15:25:06 -0300

    :bulb: (#103) Adicionando fetch dos dados por meio de parâmetro na URL

--- src/pages/ViewPrinter.js
+++ src/pages/ViewPrinter.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../style/pages/viewPrinter.css';
 import ellipse from '../assets/login_ellipse.svg';
 import voltar_vector from '../assets/voltar_vector.svg';
 import Navbar from "../components/navbar/Navbar";
+import { useParams } from "react-router-dom";
+
 
 export default function ViewPrinter(){
+    const { printerData } = useParams();
+
     // Labels dos campos de informação.
     const infoLabels = {
         numeroSerie: "Número de série",
@@ -22,27 +26,19 @@ export default function ViewPrinter(){
     }
 
     // Estado para armazenar os dados da impressora.
-    const [printer, setPrinter] = useState(
-      {
-        id: "1",
-        padrao: {
-          tipo: "Multifuncional P&B",
-          marca: "Canon",
-          modelo: "MF1643i II",
-        },
-        ip: "192.168.15.1",
-        numeroSerie: "XXXX-000000",
-        codigoLocadora: "PRINTER-004",
-        contadorInstalacao: 0,
-        ultimoContador: 0,
-        dataInstalacao: "2023-11-30T12:00:00Z",
-        dataUltimoContador: "2023-11-30T12:00:00Z",
-        contadorRetirada: 0,
-        dataRetirada: "12/10/2023",
-        unidadeId: "1",
-        status: "ATIVO"
+    const [printer, setPrinter] = useState(null);
+
+    useEffect(() => {
+      try {
+        console.log("Printer Data:", printerData);
+        const printerString = atob(printerData);
+        const printerObject = JSON.parse(printerString);
+        setPrinter(printerObject);
+      } catch (error) {
+        console.error("Error decoding Base64 string:", error);
+        // Handle the error as needed, e.g., setPrinter to a default value or show an error message.
       }
-    );
+    }, [printerData]);
 
     return(
         <>
@@ -51,23 +47,30 @@ export default function ViewPrinter(){
             <div id="viewprinter-left-content"></div>
             <div id="viewprinter-right-content">
               <div id="viewprinter-card">
-                <div id="viewprinter-info-group">
-                  <header id="viewprinter-card-header">
-                    <img alt="" src={voltar_vector}></img>
-                    <a href="/impressorascadastradas">Voltar</a>
-                  </header>
-                  <p id="viewprinter-info-header">
-                    {printer.padrao.tipo} - {printer.padrao.marca} - {printer.padrao.modelo}
-                  </p>
-                  <div id="viewprinter-info-line">
-                    {Object.entries(infoLabels).map(([key, label]) => (
-                      <div key={key} id="viewprinter-info-box">
-                        <label>{label}</label>
-                        <p>{printer?.[key]}</p>
+                
+                  {printer ? (
+                    <div id="viewprinter-info-group">
+                      <header id="viewprinter-card-header">
+                        <img alt="" src={voltar_vector}></img>
+                        <a href="/impressorascadastradas">Voltar</a>
+                      </header>
+                      <p id="viewprinter-info-header">
+                        {printer.padrao.tipo} - {printer.padrao.marca} - {printer.padrao.modelo}
+                      </p>
+                      <div id="viewprinter-info-line">
+                        {Object.entries(infoLabels).map(([key, label]) => (
+                          <div key={key} id="viewprinter-info-box">
+                            <label>{label}</label>
+                            <p>{printer?.[key]}</p>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                </div>
+                    </div>
+                    ) : (
+                      <p id="viewprinter-loading-text">Carregando dados...</p>
+                    )
+                  }
+
               </div>
               <div className="elipse-viewprinter">
                 <img alt= "elipse"  src={ellipse}></img>
