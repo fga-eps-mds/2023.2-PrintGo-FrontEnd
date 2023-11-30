b61cf71d9eed99d63b51f94b431cb58cc0c74cc4
DaviMatheus <matdavimat@gmail.com>
2023-11-30 12:58:01 -0300

    :bug:(#115): Removendo codesmell pattern list

--- src/pages/PatternList.js
+++ src/pages/PatternList.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 import "../style/pages/patternList.css";
 import { Link } from "react-router-dom";
 import Search from '../assets/Search.svg';
