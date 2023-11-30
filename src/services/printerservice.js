da9cf5eac4ae96ae5b05fa3df70448a5d19bbe67
LipeAKugel <felipearaujodff@gmail.com>
2023-11-30 19:17:28 -0300

    :bulb: (#187) Adicionando printerservice

---
+++ src/services/printerservice.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8001";
+
+export async function getPrinters() {
+  try {
+
+    const response = await axios.get(`${API_URL}/impressora`);
+    return { type: 'success', data: response.data};
+  } catch (error) {
+    return { type: 'error', error };
+  }
+}
+
+export async function getPadrao(id) {
+  try {
+    const response = await axios.get(`${API_URL}/padrao/${id}`);
+    return { type: 'success', data: response.data };
+  } catch (error) {
+    return { type: 'error', error };
+  }
+}
+
+export async function togglePrinter(id, status) {
+  const data = {
+    id,
+    status
+  }
+
+  try {
+    const response = await axios.patch(`${API_URL}/impressora`, data);
+    return { type: 'success', data: response.data };
+  } catch (error) {
+    return { type: 'error', error };
+  }
+}
