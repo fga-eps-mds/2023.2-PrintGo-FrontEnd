8fecce2dd00291b55b986f2db22a1e63634a2fd9
LipeAKugel <felipearaujodff@gmail.com>
2023-12-03 01:02:09 -0300

    :bulb: (#191) Arrumando redirecionamento da página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,10 +3,9 @@ import "../style/pages/login.css";
 import pessoas from "../assets/pessoas.svg";
 import login_ellipse from "../assets/login_ellipse.svg";
 import NavbarSimple from "../components/navbar/NavbarSimple";
-import { Link } from "react-router-dom";
 import { login } from "../api/api";
 import { useNavigate } from 'react-router-dom';
-import {decodeToken} from "react-jwt";
+import { decodeToken } from "react-jwt";
 
 
 export default function Login() {
@@ -36,13 +35,17 @@ export default function Login() {
 
     try {
       const token = await login(email, password);
+
       localStorage.setItem("jwt", token);
       const user = decodeToken(token);
-      console.log(user.nome)
-      navigate("/home");
+      console.log(user.nome);
+
+      navigate("/");
+
     } catch (error) {
       console.error(error);
       setLoginError("E-mail ou senha incorreto.");
+      
     }
   };
 
