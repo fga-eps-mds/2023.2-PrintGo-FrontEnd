beb3846cd861bf11a0e108257448fef053f1718a
LipeAKugel <felipearaujodff@gmail.com>
2023-12-06 11:51:52 -0300

    :racehorse: (#191) Corrigindo estilização da Esqueci Minha Senha

--- src/pages/ForgottenPassword.js
+++ src/pages/ForgottenPassword.js
@@ -1,15 +1,19 @@
-import "../style/pages/createUser.css";
+import "../style/pages/forgottenPassword.css";
 import React from "react";
 import signup_image from "../assets/signup_image.svg";
+import elipse from '../assets/elipse6.svg';
 import Navbar from "../components/navbar/Navbar";
 import ForgottenPasswordForm from "../components/forms/ForgottenPasswordForms";
 export default function ForgottenPasswordPage() {
   return (
     <>
       <Navbar />
-      <div id="signup-container">
+      <div id="forgotpassword-container">
         <img alt="" src={signup_image} />
         <ForgottenPasswordForm />
+        <div className="forgotpassword-elipse">
+          <img alt= "elipse"  src={elipse}></img>
+        </div>
       </div>
     </>
   );
