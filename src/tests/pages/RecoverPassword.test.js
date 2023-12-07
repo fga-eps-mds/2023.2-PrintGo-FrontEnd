aaa0119f323ac08a9c10a1cd1d50263b883cfbb4
AntonioRangelC <antonio.rangel.02@gmail.com>
2023-12-07 12:57:45 -0300

    :bulb:(#162): Adicionando teste

--- src/tests/pages/RecoverPassword.test.js
+++ src/tests/pages/RecoverPassword.test.js
@@ -6,6 +6,9 @@ import * as api from "../../api/api";
 import * as router from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
+import { toast } from 'react-toastify';
+import { recoverPassword } from "../../services/userService";
+import * as userService from '../../services/userService';
 
 
 
@@ -96,4 +99,57 @@ test('testa a alteração de senha', async () => {
     });
 
     
+});
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => ({
+    toast: {
+      error: jest.fn(),
+      success: jest.fn(),
+    },
+}));
+
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+      ...originalModule,
+      useNavigate: jest.fn(),
+    };
+});
+
+test('should display success message and redirect on successful password change', async () => {
+    const mockNavigate = jest.fn();
+    require('react-router-dom').useNavigate.mockReturnValue(mockNavigate);
+    render(
+        <BrowserRouter>
+            <RecoverPasswordPage />
+        </BrowserRouter>
+    );
+
+    
+    
+    const passwordInput = screen.getByTestId('input-nova-senha');
+    const confirmPasswordInput = screen.getByTestId('input-repita-senha');
+    const submitButton = screen.getByText('Confirmar');
+
+    //recoverPassword.mockResolvedValue({ type: 'success' });
+    jest.spyOn(userService, 'recoverPassword').mockResolvedValue({ type: 'success' });
+    jest.spyOn(toast, 'success').mockImplementation(mockToast);
+    
+    act(() => {
+        fireEvent.change(passwordInput, { target: { value: 'ValidPassword1@' } });
+        fireEvent.change(confirmPasswordInput, { target: { value: 'ValidPassword1@' } });
+    });
+
+    await act(async () => {
+        fireEvent.submit(submitButton);
+    });
+
+    // Ensure success toast is called
+    expect(toast.success).toHaveBeenCalledWith('Senha alterada com sucesso! redirecionando');
+
+    // Ensure navigate is called after the setTimeout
+    await waitFor(() => {
+        expect(window.location.pathname).toBe('/');
+        
+    });
 });
