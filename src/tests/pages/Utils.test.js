93a0a48219819594aeb44b3a24d256655b66e83c
Júlia Farias <julia.farias840@gmail.com>
2023-12-10 23:04:25 -0300

    :racehorse:#103  Corrigindo teste na utils

--- src/tests/pages/Utils.test.js
+++ src/tests/pages/Utils.test.js
@@ -1,12 +1,10 @@
-import { extractDate, formatDate } from '../../utils/utils'; 
-
-
+import { extractDate, formatDate } from '../../utils/utils';
 describe('Utilitários de Data', () => {
   describe('extractDate', () => {
     it('deve extrair e formatar a data corretamente', () => {
       const dateString = '2023-12-01T12:00:00Z';
       const result = extractDate(dateString);
-      expect(result).toBe('02/12/23'); // Note que a função adiciona +1 ao dia
+      expect(result).toBe('02/12/23'); 
     });
 
     it('deve lidar com meses de um dígito', () => {
@@ -26,7 +24,7 @@ describe('Utilitários de Data', () => {
     it('deve formatar a data corretamente para o formato AAAA-MM-DD', () => {
       const dateString = '2023-12-01T12:00:00Z';
       const result = formatDate(dateString);
-      expect(result).toBe('2023-12-02'); // Note que a função adiciona +1 ao dia
+      expect(result).toBe('2023-12-02'); 
     });
 
     it('deve lidar com meses de um dígito', () => {
