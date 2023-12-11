cbd368bb104520d254998fd0599b9a08f55f0354
Júlia Farias <julia.farias840@gmail.com>
2023-12-10 22:40:30 -0300

    :racehorse: (#103) Adicionando teste em utils

--- src/tests/utils/AdminRoutes.test.js
+++ src/tests/utils/AdminRoutes.test.js
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import AdminRoutes from '../../components/utils/AdminRoutes'; // Adjust the import path as necessary
 import { decodeToken } from "react-jwt";
+import { formatDate } from '../../components/utils/dateUtils';
 
 // Mock the 'decodeToken' and 'localStorage.getItem' functions
 jest.mock("react-jwt", () => ({
@@ -42,3 +43,23 @@ describe('AdminRoutes', () => {
   });
 });
 
+describe('formatDate', () => {
+  it('deve adicionar um zero à esquerda para meses de um dígito', () => {
+    const dateString = '2023-01-10T12:00:00Z'; // Janeiro (01) é um mês de um dígito
+    const result = formatDate(dateString);
+    expect(result).toBe('2023-01-11'); // Espera-se que adicione um zero ao mês
+  });
+
+  it('deve adicionar um zero à esquerda para dias de um dígito', () => {
+    const dateString = '2023-12-01T12:00:00Z'; // Dia 1 é um dia de um dígito
+    const result = formatDate(dateString);
+    expect(result).toBe('2023-12-02'); // Espera-se que adicione um zero ao dia
+  });
+
+  it('deve formatar corretamente datas com meses e dias de dois dígitos', () => {
+    const dateString = '2023-12-10T12:00:00Z'; // Dezembro (12) e dia 10 são de dois dígitos
+    const result = formatDate(dateString);
+    expect(result).toBe('2023-12-11'); // Nenhum zero adicional deve ser adicionado
+  });
+
+});
