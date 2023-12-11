290f579fd46b6b7993325d2eea89125a7a3ec346
Júlia Farias <julia.farias840@gmail.com>
2023-12-10 22:57:59 -0300

    :bulb: (#103) Criando teste para o arquivo utils

---
+++ src/tests/pages/Utils.test.js
@@ -0,0 +1,44 @@
+import { extractDate, formatDate } from '../../utils/utils'; 
+
+
+describe('Utilitários de Data', () => {
+  describe('extractDate', () => {
+    it('deve extrair e formatar a data corretamente', () => {
+      const dateString = '2023-12-01T12:00:00Z';
+      const result = extractDate(dateString);
+      expect(result).toBe('02/12/23'); // Note que a função adiciona +1 ao dia
+    });
+
+    it('deve lidar com meses de um dígito', () => {
+      const dateString = '2023-01-01T12:00:00Z';
+      const result = extractDate(dateString);
+      expect(result).toBe('02/01/23');
+    });
+
+    it('deve lidar com dias de um dígito', () => {
+      const dateString = '2023-12-01T12:00:00Z';
+      const result = extractDate(dateString);
+      expect(result).toBe('02/12/23');
+    });
+  });
+
+  describe('formatDate', () => {
+    it('deve formatar a data corretamente para o formato AAAA-MM-DD', () => {
+      const dateString = '2023-12-01T12:00:00Z';
+      const result = formatDate(dateString);
+      expect(result).toBe('2023-12-02'); // Note que a função adiciona +1 ao dia
+    });
+
+    it('deve lidar com meses de um dígito', () => {
+      const dateString = '2023-01-01T12:00:00Z';
+      const result = formatDate(dateString);
+      expect(result).toBe('2023-01-02');
+    });
+
+    it('deve lidar com dias de um dígito', () => {
+      const dateString = '2023-12-01T12:00:00Z';
+      const result = formatDate(dateString);
+      expect(result).toBe('2023-12-02');
+    });
+  });
+});
